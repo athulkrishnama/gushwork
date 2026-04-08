f2262aad980ad7f262f6a85710c73c5adb26ffe4
athulkrishnama24@gmail.com <athulkrishnama24@gmail.com>
2026-04-08 10:41:14 +0530

    feat: implement responsive navigation header with brand assets and Inter font integration

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
@@ -10,14 +12,10 @@ const lenis = new Lenis({
     infinite: false,
 });
 
-// Update ScrollTrigger on Lenis scroll
 lenis.on('scroll', ScrollTrigger.update);
 
-// Sync GSAP ticker with Lenis requestAnimationFrame
 gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
 });
 
-// Disable GSAP lag smoothing to fix sync issues
 gsap.ticker.lagSmoothing(0);
-
